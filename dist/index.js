5639aab0e161139dd30caef6215665b31df957fe
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-11-09 23:15:05 -0800

    commit mini project with npm start..index.html is im root file.updted json with light server

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+console.log("hello");
+function printDouble(msg) {
+    console.log(msg);
+    console.log(msg);
+}
+printDouble("howdy");
+function triplet(age, name) {
+    console.log(age);
+    console.log(name);
+    console.log(age);
+}
+triplet(23, "tony");
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+console.log("hello");
+
+function printDouble(msg: string) {
+    console.log(msg);
+    console.log(msg);
+}
+printDouble("howdy");
+
+function triplet(age: number, name: string) {
+    console.log(age);
+    console.log(name);
+    console.log(age);
+
+}
+triplet(23, "tony");
